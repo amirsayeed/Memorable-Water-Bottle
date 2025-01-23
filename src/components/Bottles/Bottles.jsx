3e9ef60483005b790ec52a79cc9df5dd9fc599b1
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-01-24 01:22:08 +0600

    add to cart functionality added

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -4,20 +4,31 @@ import './Bottles.css';
 const Bottles = () => {
 
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('./bottles.json')
             .then(response => response.json())
             .then(data => setBottles(data))
     }, []);
 
+    const handleAddToCart = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    }
+
     return (
         <div>
             <h2>
-                Bottles: {bottles.length}
+                Bottles : {bottles.length}
             </h2>
+            <p>Cart : {cart.length}</p>
             <div className="bottle-container">
                 {
-                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} />)
+                    bottles.map(bottle => <Bottle
+                        key={bottle.id}
+                        handleAddToCart={handleAddToCart}
+                        bottle={bottle}
+                    />)
                 }
             </div>
         </div>
